6ea9b472961d52c2a7fe3ff38851d3141bb901b1
JosephChan <chan.1063@osu.edu>
2025-07-09 19:20:01 -0400

    reverting time filter, but doubling the diffusion timescales

--- source/dynamical_constants.f90
+++ source/dynamical_constants.f90
@@ -13,12 +13,20 @@ module dynamical_constants
     real(p), parameter :: hscale = 7.5       ! Reference scale height for pressure (in km)
     real(p), parameter :: hshum  = 2.5       ! Reference scale height for specific humidity (in km)
     real(p), parameter :: refrh1 = 0.7       ! Reference relative humidity of near-surface air
-    real(p), parameter :: thd    = 2.4       ! Max damping time (in hours) for horizontal diffusion
-                                             ! (del^6) of temperature and vorticity
-    real(p), parameter :: thdd   = 2.4       ! Max damping time (in hours) for horizontal diffusion
-                                             ! (del^6) of divergence
-    real(p), parameter :: thds   = 12.0      ! Max damping time (in hours) for extra diffusion
-                                             ! (del^2) in the stratosphere
-    real(p), parameter :: tdrs   = 24.0*30.0 ! Damping time (in hours) for drag on zonal-mean wind
-                                             ! in the stratosphere
+    ! real(p), parameter :: thd    = 2.4       ! Max damping time (in hours) for horizontal diffusion
+    !                                          ! (del^6) of temperature and vorticity
+    ! real(p), parameter :: thdd   = 2.4       ! Max damping time (in hours) for horizontal diffusion
+    !                                          ! (del^6) of divergence
+    ! real(p), parameter :: thds   = 12.0      ! Max damping time (in hours) for extra diffusion
+    !                                          ! (del^2) in the stratosphere
+    ! real(p), parameter :: tdrs   = 24.0*30.0 ! Damping time (in hours) for drag on zonal-mean wind
+    !                                          ! in the stratosphere
+    real(p), parameter :: thd    = 2.4*2        ! Max damping time (in hours) for horizontal diffusion
+                                                ! (del^6) of temperature and vorticity
+    real(p), parameter :: thdd   = 2.4*2        ! Max damping time (in hours) for horizontal diffusion
+                                                ! (del^6) of divergence
+    real(p), parameter :: thds   = 12.0       ! Max damping time (in hours) for extra diffusion
+                                              ! (del^2) in the stratosphere
+    real(p), parameter :: tdrs   = 24.0*30.0  ! Damping time (in hours) for drag on zonal-mean wind
+                                              ! in the stratosphere
 end module

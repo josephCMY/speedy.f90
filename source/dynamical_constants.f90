02c5cbf0a5e38abda5f63c00a0f751797539daf1
JosephChan <chan.1063@osu.edu>
2025-07-10 23:12:14 -0400

    reverting damping time

--- source/dynamical_constants.f90
+++ source/dynamical_constants.f90
@@ -27,6 +27,6 @@ module dynamical_constants
                                                  ! (del^6) of divergence
     real(p), parameter :: thds   = 12.0*20       ! Max damping time (in hours) for extra diffusion
                                               ! (del^2) in the stratosphere
-    real(p), parameter :: tdrs   = 24.0*30.0*20  ! Damping time (in hours) for drag on zonal-mean wind
+    real(p), parameter :: tdrs   = 24.0*30.0  ! Damping time (in hours) for drag on zonal-mean wind
                                               ! in the stratosphere
 end module

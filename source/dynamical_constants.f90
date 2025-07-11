487fcf2001906204409bc051397c30e3bf7bd072
JosephChan <chan.1063@osu.edu>
2025-07-11 16:04:17 -0400

    sticking to 20x diffusion time scale instead of 200x

--- source/dynamical_constants.f90
+++ source/dynamical_constants.f90
@@ -21,11 +21,11 @@ module dynamical_constants
     !                                          ! (del^2) in the stratosphere
     ! real(p), parameter :: tdrs   = 24.0*30.0 ! Damping time (in hours) for drag on zonal-mean wind
     !                                          ! in the stratosphere
-    real(p), parameter :: thd    = 2.4*200        ! Max damping time (in hours) for horizontal diffusion
+    real(p), parameter :: thd    = 2.4*20        ! Max damping time (in hours) for horizontal diffusion
                                                   ! (del^6) of temperature and vorticity
-    real(p), parameter :: thdd   = 2.4*200        ! Max damping time (in hours) for horizontal diffusion
+    real(p), parameter :: thdd   = 2.4*20        ! Max damping time (in hours) for horizontal diffusion
                                                   ! (del^6) of divergence
-    real(p), parameter :: thds   = 12.0*200       ! Max damping time (in hours) for extra diffusion
+    real(p), parameter :: thds   = 12.0*20       ! Max damping time (in hours) for extra diffusion
                                               ! (del^2) in the stratosphere
     real(p), parameter :: tdrs   = 24.0*30.0  ! Damping time (in hours) for drag on zonal-mean wind
                                               ! in the stratosphere
